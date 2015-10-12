9e2bb96c06c82a7024454705093df84d31280c83
meiraka <meiraka.pre@gmail.com>
2015-10-12 14:38:35 +0900

    add prev/next album function

--- bin/mpd-button.py
+++ bin/mpd-button.py
@@ -9,22 +9,120 @@ import subprocess
 import time
 import logging
 import traceback
+import threading
 import signal
 import sys
+
+
+class MPD(threading.Thread):
+
+    """ mpd controller."""
+
+    def __init__(self):
+        """Initialize thread."""
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.playlist = []
+        self.start()
+
+    def run(self):
+        """Get playlist."""
+        while True:
+            try:
+                out = subprocess.check_output(
+                    ['/usr/bin/mpc', 'playlist', '-f', '%album%'])
+                self.playlist = [{'album': i} for i in out.split()]
+
+                subprocess.check_output(
+                    ['/usr/bin/mpc', 'idle', 'playlist'])
+            except subprocess.CalledProcessError:
+                time.sleep(1)
+
+    def prev_album(self):
+        """Play prev album song."""
+        pos = self._position()
+        current_album = self.playlist[pos]['album']
+        use_prev_album = False
+        prev_album = None
+        for prev_count, song in enumerate(reversed(self.playlist[:pos])):
+            if prev_count == 0 and not current_album == song['album']:
+                use_prev_album = True
+                continue
+            if not use_prev_album:
+                if not current_album == song['album']:
+                    subprocess.check_output(
+                        ['/usr/bin/mpc', 'play',
+                         str(pos - prev_count + 1)])
+                    return
+            else:
+                if prev_album is None:
+                    if not current_album == song['album']:
+                        prev_album = song['album']
+                        print prev_album
+                else:
+                    if not prev_album == song['album']:
+                        subprocess.check_output(
+                            ['/usr/bin/mpc', 'play',
+                             str(pos - prev_count + 1)])
+                        return
+        subprocess.check_output(
+            ['/usr/bin/mpc', 'play', '1'])
+
+    def prev(self):
+        """Play prev song."""
+        subprocess.check_output(
+            ['/usr/bin/mpc', 'prev'])
+
+    def pause(self):
+        """Pause song."""
+        subprocess.check_output(
+            ['/usr/bin/mpc', 'pause'])
+
+    def play(self):
+        """Play song."""
+        subprocess.check_output(
+            ['/usr/bin/mpc', 'play'])
+
+    def next(self):
+        """Play next song."""
+        subprocess.check_output(
+            ['/usr/bin/mpc', 'next'])
+
+    def next_album(self):
+        """Play prev album song."""
+        pos = self._position()
+        current_album = self.playlist[pos]['album']
+        for next_count, song in enumerate(self.playlist[pos:]):
+            if not current_album == song['album']:
+                subprocess.check_output(
+                    ['/usr/bin/mpc', 'play', str(pos + next_count + 1)])
+                return
+        subprocess.check_output(
+            ['/usr/bin/mpc', 'play', '1'])
+
+    def _position(self):
+        """Return playlist playing position."""
+        out = subprocess.check_output(
+            ['/usr/bin/mpc', '-f', '%position%'])
+        return int(out.splitlines()[0]) - 1
 
 
 class App(object):
 
     """RotarySwitch for mpd control."""
 
-    def __init__(self, prev, pause, play, next, logger=None):
+    def __init__(self, prev_album, prev, pause, play, next, next_album,
+                 logger=None):
         """Open gpio for prev/play/next button."""
+        self.mpd = MPD()
         self.logger = logger if logger else logging
         self.logger.info("start app")
+        self._prev_album = prev_album
         self._prev = prev
         self._pause = pause
         self._play = play
         self._next = next
+        self._next_album = next_album
         self._last = self._pause
         signal.signal(signal.SIGTERM, self.exit)
         signal.signal(signal.SIGINT, self.exit)
@@ -34,9 +132,17 @@ class App(object):
         self.logger.info("stop app")
         sys.exit(0)
 
+    def _gpio_read(self, f):
+        """read gpio value."""
+        f.seek(0)
+        out = f.read().strip()
+        return out
+
     def run(self):
         """Wait gpio value is changed."""
         epoll = select.epoll()
+        epoll.register(self._prev_album,
+                       select.EPOLLIN | select.EPOLLET)
         epoll.register(self._prev,
                        select.EPOLLIN | select.EPOLLET)
         epoll.register(self._pause,
@@ -45,28 +151,51 @@ class App(object):
                        select.EPOLLIN | select.EPOLLET)
         epoll.register(self._next,
                        select.EPOLLIN | select.EPOLLET)
+        epoll.register(self._next_album,
+                       select.EPOLLIN | select.EPOLLET)
         try:
             while True:
                 try:
+                    time.sleep(0.1)
                     for fileno, event in epoll.poll():
-                        if fileno == self._prev.fileno():
-                            subprocess.check_output(
-                                ['/usr/bin/mpc', 'prev'])
-                        if fileno == self._pause.fileno():
-                            subprocess.check_output(
-                                ['/usr/bin/mpc', 'pause'])
-                        if fileno == self._play.fileno():
-                            subprocess.check_output(
-                                ['/usr/bin/mpc', 'play'])
-                        if fileno == self._next.fileno():
-                            subprocess.check_output(
-                                ['/usr/bin/mpc', 'next'])
+                        if self._gpio_read(self._prev_album) == '1':
+                            if not self._last == self._prev_album:
+                                self.mpd.prev_album()
+                            self._last = self._prev_album
+                        if self._gpio_read(self._prev) == '1':
+                            if self._last not in [self._prev_album,
+                                                  self._prev]:
+                                self.mpd.prev()
+                            self._last = self._prev
+                        if self._gpio_read(self._pause) == '1':
+                            if (self._last not in [self._prev,
+                                                   self._prev_album]):
+                                self.mpd.pause()
+                            self._last = self._pause
+                        if self._gpio_read(self._play) == '1':
+                            self.mpd.play()
+                            self._last = self._play
+                        if self._gpio_read(self._next_album) == '1':
+                            if not self._last == self._next_album:
+                                self.mpd.next_album()
+                            self._last = self._next_album
+                        if self._gpio_read(self._next) == '1':
+                            if self._last not in [self._next,
+                                                  self._next_album]:
+                                self.mpd.next()
+                            self._last = self._next
+
                 except subprocess.CalledProcessError:
                     time.sleep(1)
+                except IndexError:
+                    time.sleep(1)
         finally:
+            epoll.unregister(self._prev_album)
             epoll.unregister(self._prev)
+            epoll.unregister(self._pause)
             epoll.unregister(self._play)
             epoll.unregister(self._next)
+            epoll.unregister(self._next_album)
 
 
 def gpio_open(port, mode='r', register='', edge='none', active_low='0'):
@@ -107,19 +236,14 @@ def main():
         datefmt='%Y/%m/%d %H:%M:%S',
         level=logging.DEBUG)
     logger = logging.getLogger(__name__)
-    try:
-        # prev_album = gpio_open(22, edge='rising')
-        prev = gpio_open(10, edge='rising')
-        pause = gpio_open(9, edge='rising')
-        play = gpio_open(11, edge='rising')
-        next = gpio_open(23, edge='rising')
-        # next_album = gpio_open(24, edge='rising')
-        sw = App(prev, pause, play, next, logger)
-        sw.run()
-        # sw.run_poll()
-    except Exception, err:
-        logger.critical("app exit with: %s" % str(err))
-        logger.critical(traceback.format_exc())
+    prev_album = gpio_open(22, edge='rising')
+    prev = gpio_open(10, edge='rising')
+    pause = gpio_open(9, edge='rising')
+    play = gpio_open(11, edge='rising')
+    next = gpio_open(23, edge='rising')
+    next_album = gpio_open(24, edge='rising')
+    sw = App(prev_album, prev, pause, play, next, next_album, logger)
+    sw.run()
 
 if __name__ == '__main__':
     main()
